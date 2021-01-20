f1b750877fe4eb4f6853b9082a7b3ead79883c26
Jacques Pagels <jacques.pagels@hotmail.com>
2021-01-20 11:33:43 +0000

    Adding more styling

---
+++ src/app/components/fruit-tile/fruit-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fruit-tile',
+  templateUrl: './fruit-tile.component.html',
+  styleUrls: ['./fruit-tile.component.scss']
+})
+export class FruitTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/index.ts
@@ -0,0 +1,2 @@
+export { BasketComponent } from './basket/basket.component';
+export { FruitTileComponent } from './fruit-tile/fruit-tile.component';
